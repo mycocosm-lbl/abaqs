3f5f228c391ad014694c81493fe0abbb630fa7b8
ishabalov <ishabalov@lbl.gov>
2026-03-11 15:37:55 -0700

    Minor code cleanup

--- src/main/java/org/mycocosm/abaqs/main/ABAQS.java
+++ src/main/java/org/mycocosm/abaqs/main/ABAQS.java
@@ -50,8 +50,6 @@ import org.mycocosm.framework.logger.LoggerFactory;
 import org.mycocosm.framework.logger.LoggerHelper;
 import org.mycocosm.framework.text.PatternHelper;
 import org.mycocosm.framework.text.TextHelper;
-//import org.apache.commons.lang.mutable.MutableDouble;
-//import org.apache.commons.lang3.mutable.MutableInt;
 import org.mycocosm.gff3.GFF3Data;
 import org.mycocosm.gff3.Gff3Record;
 import org.mycocosm.gff3.Gff3Type;
@@ -107,7 +105,6 @@ public class ABAQS implements BatchRunnableCli {
 	public static final CliOption<Path> GENE_CODE_FILE = CliOption.optionalPathWithArgument("igc", "gene-code-input-file", "Gene code input file (gc.prt), if missing internal copy will be used");
 
 	private final Logger logger;
-	//	@Inject NcbiGeneCodeCache geneCodeFactory;
 
 	public static final String VERSION = "1.0"; 
 
